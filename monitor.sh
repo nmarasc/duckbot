886c0e17e4e4db56fbe36fd323526cd34e0f911f
Nick Marasco <nicdmarasco@gmail.com>
2018-03-13 01:56:10 +0000

    modified monitor.sh to be for duckbot

--- monitor.sh
+++ monitor.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Get python install location
-python=`which python3 2> /dev/null`
-if [ $? -ne 0 ]; then
-  python=`which python`
-fi
 msg_headers=("MONITOR" "UPDATE" "ERROR")
 return_code=0
 retry=0
@@ -19,7 +14,7 @@ function post_msg {
 # Bot startup function
 function start_bot {
   return_code=0
-  $python main.py "$bot_status" & return_code=$?;pylon_pid=$!
+  python duckbot "$bot_status" & return_code=$?;duck_pid=$!
   # RC!=0 , something bad happened with bash
   if [ $return_code -ne 0 ]; then
     if [ $retry -eq 1 ]; then
@@ -42,41 +37,26 @@ while sleep 1; do
     if [ $return_code -eq 0 ]; then
       retry=0
       # Wait for finish and grab return code
-      post_msg ${msg_headers[0]} "pylon_factory.py started with pid: $pylon_pid"
-      wait "$pylon_pid"; pylon_exit=$?
+      post_msg ${msg_headers[0]} "duckbot started with pid: $duck_pid"
+      wait "$duck_pid"; duck_exit=$?
 
       # Check return code
       # RC=0 , clean exit
-      if [ $pylon_exit -eq 0 ]; then
-        post_msg ${msg_headers[0]} "pylon_factory.py exited with RC=$pylon_exit"
+      if [ $duck_exit -eq 0 ]; then
+        post_msg ${msg_headers[0]} "duckbot exited with RC=$duck_exit"
         post_msg ${msg_headers[0]} "Script terminating"
         exit 0
 
       # RC=1 , Uncaught python error, python doesn't give good return codes, so
       # give up, don't even try again, just quit, end it
-      elif [ $pylon_exit -eq 1 ]; then
-        post_msg ${msg_headers[2]} "pylon_factory.py failed with RC=$pylon_exit"
+      elif [ $duck_exit -eq 1 ]; then
+        post_msg ${msg_headers[2]} "duckbot failed with RC=$duck_exit"
         post_msg ${msg_headers[2]} "Some uncaught error slipped through"
         exit 1
-#         git revert --no-commit HEAD~1
-#         git commit -m "AUTO: Reverting to previous commit"; return_code=$?
-#         if [ $return_code -eq 0 ]; then
-#           git push; return_code=$?
-#           if [ $return_code -eq 0 ]; then
-#             post_msg ${msg_headers[1]} "Sucessfully reverted, restarting bot"
-#             return_code=1
-#           else
-#             post_msg ${msg_headers[2]} "Unable to push reversion, hep"
-#             exit 1
-#           fi
-#         else
-#           post_msg ${msg_headers[2]} "Unable to revert or commit change, hep"
-#           exit 1
-#         fi
 
       # RC=2 , clean exit and update
-      elif [ $pylon_exit -eq 2 ]; then
-        post_msg ${msg_headers[0]} "pylon_factory.py exited with RC=$pylon_exit"
+      elif [ $duck_exit -eq 2 ]; then
+        post_msg ${msg_headers[0]} "duckbot exited with RC=$duck_exit"
         post_msg ${msg_headers[1]} "Beginning update"
         git pull; return_code=$?
 
@@ -90,7 +70,7 @@ while sleep 1; do
 
       # RC=? , unknown return code from bot, attempt to restart
       else
-        post_msg ${msg_headers[2]} "Unrecognized RC=$pylon_exit, attempting restart"
+        post_msg ${msg_headers[2]} "Unrecognized RC=$duck_exit, attempting restart"
         bot_status=""
       fi
     fi
