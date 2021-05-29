86cfb6f68cb9e06eb393835591f68be36938efa3
Nick Marasco <nicdmarasco@gmail.com>
2021-05-28 22:40:32 -0400

    Fixed roll command b/c shadowing

--- duckbot/cogs/random.py
+++ duckbot/cogs/random.py
@@ -14,7 +14,7 @@ from discord.ext import commands
 
 from duckbot.util import etoi
 
-from .util import roll
+from .util import roll as doRoll
 from .util.data import base_ratings
 from .util.data import eightball_messages
 
@@ -69,41 +69,41 @@ class Random(commands.Cog):
         ignore_extra=True,
         aliases=[':game_die:']
     )
-    async def roll(self, ctx, roll=None):
+    async def roll(self, ctx, input=None):
         r"""Random dice rolling.
 
         Parameters
         ----------
         ctx
             Context information for the command
-        roll : optional
+        input : optional
             user argument to roll command
         """
-        logger.debug(f'Roll command called with: {roll}')
+        logger.debug(f'Roll command called with: {input}')
 
         response = ''
         botmoji = str(ctx.bot.emoji)
         amount = 1
 
-        if roll is None:
+        if input is None:
             response = f'You probably meant roll 20, kweh! {botmoji}\n'
-            roll = '20'
+            input = '20'
 
-        regex_result = re.search(self._roll_regex, roll)
+        regex_result = re.search(self._roll_regex, input)
 
         if regex_result:  # if there was a match
             if regex_result.group(1):  # if there's a Y value
                 amount = self._parseNumOrEmoji(regex_result.group(1))
             sides = self._parseNumOrEmoji(regex_result.group(2))  # X value
         else:  # No match, just a number (or emoji)
-            sides = self._parseNumOrEmoji(roll)
+            sides = self._parseNumOrEmoji(input)
 
         if (type(amount) == str or amount not in self._die_range_num):
             response += f'{amount} is not in the valid range'
         elif (type(sides) == str or sides not in self._die_range_side):
             response += f'{sides} is not in the valid range'
         else:
-            result = roll(sides, amount)
+            result = doRoll(sides, amount)
             if isinstance(result, list):
                 head = ', '.join(map(str, result))
                 tail = f'\nYour total: {sum(result)}'
@@ -127,7 +127,7 @@ class Random(commands.Cog):
         ctx
             Context information for the command
         """
-        result = roll(2)
+        result = doRoll(2)
         if result == 1:
             message = 'You got: HEADS'
         else:
@@ -151,7 +151,7 @@ class Random(commands.Cog):
             list of things to pick from
         """
         if len(picks) in self._pick_range:
-            pick = picks[roll(len(picks)-1)]
+            pick = picks[doRoll(len(picks)-1)]
             response = f'I choose: {pick}'
         else:
             botmoji = str(ctx.bot.emoji)
@@ -175,7 +175,7 @@ class Random(commands.Cog):
         ctx
             Context information for the command
         """
-        message = eightball_messages[roll(len(eightball_messages)-1)]
+        message = eightball_messages[doRoll(len(eightball_messages)-1)]
         await ctx.send(f'{ctx.message.author.mention} {message} {ctx.bot.emoji}')
 
     async def cog_command_error(self, ctx, error):
@@ -197,21 +197,21 @@ class Random(commands.Cog):
             value = etoi(value)
         return value
 
-    def _emojiRating(self, roll, die):
+    def _emojiRating(self, score, die):
         r"""Give an emoji rating based on roll score.
 
         Parameters
         ----------
-        roll
+        score
             score to rate
         die
             maximum score to compare
         """
-        emoji = base_ratings.get(roll, None)
-        if roll == die:
+        emoji = base_ratings.get(score, None)
+        if score == die:
             emoji = ':tada:'
         elif emoji is None:
-            if roll <= die/2:
+            if score <= die/2:
                 emoji = ':-1:'
             else:
                 emoji = ':ok_hand:'
