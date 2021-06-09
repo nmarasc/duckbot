89d7e13a9997eb0fc0a7d5f3ae62302f915e3dee
Nick Marasco <nicdmarasco@gmail.com>
2021-06-09 18:50:32 -0400

    Deleting legacy code from old version

--- duckbot/util/choiceFunctions.py
+++
@@ -1,8 +0,0 @@
-# Last Updated: 2.3.0
-import random
-
-# Randomly get user to steal from
-# Params: players - dict of user bank info
-# Return: player entry to steal from
-def randomChoice(players):
-    return players[random.randrange(0,len(players))]
